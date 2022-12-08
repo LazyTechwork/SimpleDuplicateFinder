648cea67d148adde8d7f6ed9ef5bca6b3a8f48b7
Ivan Petrov <lazytechwork@gmail.com>
2022-12-08 14:13:02 +0300

    Пропускаем файлы больше 2ГБ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,19 @@
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
 
 fun main() {
     val hashes = HashMap<ULong, ArrayList<File>>()
     print("Введите путь до директории: ")
     val dir = File(readln())
     dir.walkTopDown().forEach {
-        if (it.isFile && it.canRead()) try {
-            val hash = hash64FNV1a(it.readBytes())
-            hashes[hash] = hashes.getOrDefault(hash, ArrayList()).apply { add(it) }
-        } catch (_: IOException) {
+        if (it.isFile && it.canRead()) {
+            if (it.length() < 2L * 1024 * 1024 * 1024) try {
+                val hash = hash64FNV1a(it.readBytes())
+                hashes[hash] = hashes.getOrDefault(hash, ArrayList()).apply { add(it) }
+            } catch (_: IOException) {
+            }
+            else println("Пропущен анализ файла %s из-за большого размера".format(it.toRelativeString(dir)))
         }
     }
 
