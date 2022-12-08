d093b3c745625f0e15b3a3388fa907cc20fc23ae
Ivan Petrov <lazytechwork@gmail.com>
2022-12-08 14:06:57 +0300

    Фикс говна

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,26 +3,23 @@ import java.io.IOException
 
 fun main() {
     val hashes = HashMap<ULong, ArrayList<File>>()
-    val dir = File(".")
+    print("Введите путь до директории: ")
+    val dir = File(readln())
     dir.walkTopDown().forEach {
-        if (it.isFile && it.canRead())
-            try {
-                val hash = hash64FNV1a(it.readBytes())
-                hashes[hash] = hashes.getOrDefault(hash, ArrayList()).apply { add(it) }
-            } catch (_: IOException) {
-            }
+        if (it.isFile && it.canRead()) try {
+            val hash = hash64FNV1a(it.readBytes())
+            hashes[hash] = hashes.getOrDefault(hash, ArrayList()).apply { add(it) }
+        } catch (_: IOException) {
+        }
     }
 
     hashes.filter { it.value.size > 1 }.forEach {
-        System.out.println(
-            "Found %d duplicate(s) for file %s (%s):".format(
-                it.value.size - 1,
-                it.value.first().toRelativeString(dir),
-                it.key.toString()
+        println(
+            "Найдено %d дубликат(-ов) для файла %s (хеш-сумма: %s):".format(
+                it.value.size - 1, it.value.first().toRelativeString(dir), it.key.toString()
             )
         )
-        it.value.subList(1, it.value.size).forEach { file -> System.out.println(file.toRelativeString(dir)) }
-        System.out.println()
+        it.value.subList(1, it.value.size).forEach { file -> println(file.toRelativeString(dir)) }
+        println()
     }
 }
-
